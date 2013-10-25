50698890e1931ad6313c08ff7cac6f7788c605e4
root <root@Debian>
2013-10-25 07:41:15 -0700

    initial commit with framework

---
+++ restCall/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RestCall::Application.config.secret_key_base = '11877f21b57f8a480a01104f0569118ae5920cb7c0e04e2dc4470c58eb9c6443b349f6ce63866197cc9a054892c377a0ff7fed6dcf2d873408d27e70b49c3090'
